75d4bbfd23f930ea0d9b943eaae8696168c45f08
George Eng <georgeeng89@gmail.com>
2016-11-14 18:15:13 -0800

    Cookie middleware

--- app.js
+++ app.js
@@ -2,24 +2,20 @@ var express = require("express")
 var app = express()
 var routes = require("./routes/index")
 var cookieParser = require('cookie-parser')
-var cookieSession = ('cookie-session')
+var cookieSession = require('cookie-session')
 var bodyParser = require('body-parser')
 const PORT = 1337;
-
-<<<<<<< HEAD
-app.use('/profile', routes.profile);
-app.use('/createProfile', routes.createProfile);
-=======
-app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({
   extended: true
 }))
-
 app.use(cookieParser());
+
+app.set("view engine", "ejs");
+
+
 app.use('/createProfile', routes.createProfile);
 app.use('/profile', routes.profile);
->>>>>>> 887dbfdad99165b20978f563673c97b8999d986e
 app.use('/friends',routes.friends);
 app.use('/upload', routes.upload);
 
