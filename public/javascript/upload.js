08bb0c563f4151068c5b08f566b8655f56041968
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2016-11-16 17:49:11 -0800

    still working on the exif info

--- public/javascript/upload.js
+++ public/javascript/upload.js
@@ -15,14 +15,12 @@ window.onload = function () {
   });
 
   dropzone.on('drop', function(e) {
-    console.log(e);
     e.stopPropagation();
     e.preventDefault();
     $(this).removeClass('dropzoneDrop');
 
     var files = e.originalEvent.dataTransfer.files;
-    console.log(Array.isArray(files));
-    var imagesArr = []
+    console.log(files);
     for (var i = 0; i < files.length; i++) {
       console.log('------------FILE-----------------');
       var file = files[i];
@@ -33,42 +31,62 @@ window.onload = function () {
         console.log(key + ": " + file[key]);
 
       }
-      toDataUrl('http://ghk.h-cdn.co/assets/cm/15/12/55091a87c39e4-0713-breville-blender-xln.jpg', function(base64Img) {
-        console.log(base64Img);
-      });
+      console.log(e.originalEvent.dataTransfer.getData(file.type));
+      var fileReader = new FileReader();
+
+      fileReader.onload = function (event) {
+        var url = event.target.result;
+        console.log("*************************");
+        console.log(event.target === this);
+        var image = document.createElement('img');
+        document.body.appendChild(image);
+        image.src = url;
+        console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        // console.log(url);
+      }
+      fileReader.readAsDataURL(file);
+
+
     }
   });
 
-  function toDataUrl(src, callback, outputFormat) {
-    var img = new Image();
-    img.crossOrigin = 'Anonymous';
-    img.onload = function() {
-      var canvas = document.createElement('CANVAS');
-      var ctx = canvas.getContext('2d');
-      var dataURL;
-      canvas.height = this.height;
-      canvas.width = this.width;
-      ctx.drawImage(this, 0, 0);
-      dataURL = canvas.toDataURL(outputFormat);
-      callback(dataURL);
-    };
-    img.src = src;
-    if (img.complete || img.complete === undefined) {
-      img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
-      img.src = src;
-    }
-  }
-
-  // function upload(file){
-  //   xhr = new XMLHttpRequest();
-  //   //initiate request
-  //   xhr.open('post', 'file to send', true);
-  //
-  //   xhr.setRequestHeader('Content-Type', 'multipart/form-data');
-  //   xhr.setRequestHeader('X-File-Name', file.fileName)
-  //   console.log(file.name);
-  //
-  //   //send file
-  //   xhr.send(file);
-  // }
 };
+
+//=========NOT NEEDED AT THE MOMENT=================
+
+// toDataUrl('http://ghk.h-cdn.co/assets/cm/15/12/55091a87c39e4-0713-breville-blender-xln.jpg', function(base64Img) {
+//   console.log(base64Img);
+// });
+
+// function toDataUrl(src, callback, outputFormat) {
+//   var img = new Image();
+//   img.crossOrigin = 'Anonymous';
+//   img.onload = function() {
+//     var canvas = document.createElement('CANVAS');
+//     var ctx = canvas.getContext('2d');
+//     var dataURL;
+//     canvas.height = this.height;
+//     canvas.width = this.width;
+//     ctx.drawImage(this, 0, 0);
+//     dataURL = canvas.toDataURL(outputFormat);
+//     callback(dataURL);
+//   };
+//   img.src = src;
+//   if (img.complete || img.complete === undefined) {
+//     img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+//     img.src = src;
+//   }
+// }
+
+// function upload(file){
+//   xhr = new XMLHttpRequest();
+//   //initiate request
+//   xhr.open('post', 'file to send', true);
+//
+//   xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+//   xhr.setRequestHeader('X-File-Name', file.fileName)
+//   console.log(file.name);
+//
+//   //send file
+//   xhr.send(file);
+// }
--- routes/upload.js
+++ routes/upload.js
@@ -1,31 +1,92 @@
 "use strict"
-
 var express = require("express")
 var router = express.Router();
-
-//---------Multer-----------------
+var ExifImage = require('exif').ExifImage;
+//---------Not sure if this is needed
 var multer = require('multer');
 var storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, 'public/uploads');
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.originalname);
-	}
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
 });
 var upload = multer({ storage: storage });
 //----------------------------------------
-
-
 router.route('/').get(function(req,res){
-	res.render('upload')
-	// res.send("Upload an image");
+    res.render('upload')
+    // res.send("Upload an image");
+});
+router.post('/', upload.any(), function (req, res, next) {
+    res.send(filePath);
+
+	for (var i = 0; i < req.files.length; i++) {
+		var filePath = req.files[i].path;
+		var imageExifData = [];
+		try {
+			new ExifImage({ image : filePath }, function (error, exifData) {
+				// console.log('this is the exif data', exifData);
+				if (error) {
+					console.log('Error: ' + error.message);
+				}
+				else {
+					imageExifData.push(exifData);
+					// console.log('this is the metaData', exifData); // Do something with your data!
+				}
+			});
+		} catch (error) {
+			console.log('Error: ' + error.message);
+		}//End of the try
+	}//End of the for loop
+console.log(imageExifData);
+
 });
 
-router.post('/', upload.any(), function (req, res, next) {
-	console.log(req.files);
-	console.log(req.files[0].path);
-	res.send(req.files);
-});
+
+//---Functions for data extractions---
+function imageDataExtraction1(reqData) {
+	var imageDataArr = [];
+	var uploadImagesData = reqData.files;
+	for (let i = 0; i < uploadImagesData.length; i++) {
+		imageDataArr.push(uploadImagesData[i]);
+	}
+	return imageDataArr;
+}
+
+//---NOT NEEDED---
+// function toDataUrl(url, callback) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.responseType = 'blob';
+//   xhr.onload = function() {
+//     var reader = new FileReader();
+//     reader.onloadend = function() {
+//       callback(reader.result);
+//     }
+//     reader.readAsDataURL(xhr.response);
+//   };
+//   xhr.open('GET', url);
+//   xhr.send();
+// }
+
+// function toDataUrl(src, callback, outputFormat) {
+//   var img = new Image();
+//   img.crossOrigin = 'Anonymous';
+//   img.onload = function() {
+//     var canvas = document.createElement('CANVAS');
+//     var ctx = canvas.getContext('2d');
+//     var dataURL;
+//     canvas.height = this.height;
+//     canvas.width = this.width;
+//     ctx.drawImage(this, 0, 0);
+//     dataURL = canvas.toDataURL(outputFormat);
+//     callback(dataURL);
+//   };
+//   img.src = src;
+//   if (img.complete || img.complete === undefined) {
+//     img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+//     img.src = src;
+//   }
+// }
 
 module.exports = router;
